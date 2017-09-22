3d70b687aa660f1758cd9a55176b5f5550410a03
Jonathan E. McMahon <jmcmahon32@gatech.edu>
2017-09-22 10:41:13 -0400

    Adding tests for other helper functions

--- test_rsa.py
+++ test_rsa.py
@@ -1,3 +1,4 @@
+"""Unit tests for RSA cryptosystem and related modules."""
 import unittest
 import helpers as util
 import rsa
@@ -13,6 +14,20 @@ class TestUtilFunctions(unittest.TestCase):
         self.assertEqual(util.relatively_prime(3, 62), True)
         self.assertEqual(util.relatively_prime(5, 23), True)
         self.assertEqual(util.relatively_prime(21, 91), False)
+
+    def test_extended_euclid(self):
+        assert util.extended_euclid(20, 79) == (4.0, -1.0, 1)
+        x, y, d = util.extended_euclid(3, 62)
+        assert (3 * x) % 62 == 1
+        x, y, d = util.extended_euclid(5, 23)
+        assert (5 * x) % 23 == 1
+
+    def test_mult_mod_inverse(self):
+        assert util.mod_mult_inverse(20, 79) == 4.0
+        assert util.mod_mult_inverse(3, 62) == 21.0
+        assert util.mod_mult_inverse(5, 23) == 14.0
+        assert util.mod_mult_inverse(21, 91) is None
+
 
 if __name__ == '__main__':
     unittest.main()
