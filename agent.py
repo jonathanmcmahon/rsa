142208f5e8f8751c5d74859575a1dc1ef4aa1651
Jonathan E. McMahon <jmcmahon32@gatech.edu>
2018-06-30 19:46:38 -0400

    Add public methods to agent

--- agent.py
+++ agent.py
@@ -1,8 +1,16 @@
 """An agent that can send and receive messages."""
-
+import rsa as crypto
 
 class Agent:
 
-    def __init__(self):
-        pass
+    def __init__(self, c=None):
+        self.c = c if c else crypto.RSA(8)
 
+    def encrypt_message(self, m, public_key):
+        return self.c.encrypt(m, public_key)
+
+    def decrypt_message(self, m):
+        return self.c.decrypt(m)
+
+    def get_public_key(self):
+        return self.c.get_public_key()
