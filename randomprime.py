ac30293b641584f8fbc66bd101d21465532c6836
Jonathan E. McMahon <jmcmahon32@gatech.edu>
2018-06-30 19:48:15 -0400

    Implement RandomPrimeGenerator

--- randomprime.py
+++ randomprime.py
@@ -1,8 +1,9 @@
 """A random prime number generator."""
+import random
 
 
 def prime_test_naive(num):
-    for i in range(2, num):
+    for i in range(2, (num / 2) + 1):
         if num % i == 0:
             return False
     return True
@@ -15,10 +16,11 @@ class RandomPrimeGenerator:
 
     @staticmethod
     def generate(bits):
-        # max = (2 ** bits) - 1
-        # while prime.isprime()
-        # return prime
-        return 0b00001101
+        max = 2 ** bits
+        p = 4
+        while not RandomPrimeGenerator.is_prime(p):
+            p = random.randint(3, max)
+        return p
 
     @staticmethod
     def is_prime(num, f=prime_test_naive):
